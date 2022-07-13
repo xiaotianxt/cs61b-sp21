aade3018a0e8055a8c2c4e2e4eb25956b7e40c32
xiaotianxt <tianyp@pku.edu.cn>
2022-07-13 21:21:18 +0800

    rename test file

--- proj2/gitlet/BlobLinkedListTest.java
+++ proj2/gitlet/BlobLinkedListTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 import java.io.File;
 
-public class FileTreeTest {
+public class BlobLinkedListTest {
     @Test
-    public void testFileTreeLoad() {
+    public void testBlobLoad() {
         BlobLinkedList tree = new BlobLinkedList(new File("hello"));
         System.out.println(tree);
     }
